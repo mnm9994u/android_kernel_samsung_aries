04f6b670f858ff56acf8251ffa458b7cb2953f80
Stratos Karafotis <stratosk@semaphore.gr>
2013-05-09 23:44:18 +0300

    sema_autobr: max_br_threshold can be 0
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>

--- drivers/video/samsung/sema_autobr.c
+++ drivers/video/samsung/sema_autobr.c
@@ -192,7 +192,7 @@ static ssize_t store_max_br_threshold(struct device *dev,
 	int ret;
 
 	ret = sscanf(buf, "%hu", &input);
-	if (ret != 1 || input < 1 || input > DRV_MAX_LUX)
+	if (ret != 1 || input < 0 || input > DRV_MAX_LUX)
 		return -EINVAL;
 
 	sa_tuners.max_br_threshold = input;
